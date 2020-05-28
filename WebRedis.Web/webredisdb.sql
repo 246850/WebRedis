0e697adb863e1e7d84a38fd0697948c50fe820fc
jianghaibo <390915549@qq.com>
2020-05-28 16:44:01 +0800

    数据库文件

--- WebRedis.Web/webredisdb.sql
+++ WebRedis.Web/webredisdb.sql
@@ -29,15 +29,3 @@ CREATE TABLE IF NOT EXISTS `connection_config` (
   `CreateTime` datetime NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='Redis连接表';
-
--- Dumping data for table webredis.connection_config: ~0 rows (大约)
-DELETE FROM `connection_config`;
-/*!40000 ALTER TABLE `connection_config` DISABLE KEYS */;
-INSERT INTO `connection_config` (`Id`, `Name`, `IpAddress`, `Port`, `Password`, `Description`, `CreateTime`) VALUES
-	(5, '172.16.10.246', '172.16.10.246', 6379, NULL, '172.16.10.246', '2020-05-28 16:32:08'),
-	(6, '101.132.140.8	', '101.132.140.8', 6379, NULL, '101.132.140.8	', '2020-05-28 16:34:14');
-/*!40000 ALTER TABLE `connection_config` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
